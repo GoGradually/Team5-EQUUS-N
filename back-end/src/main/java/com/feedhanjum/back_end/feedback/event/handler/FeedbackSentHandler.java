4a9ecc3c656a6dc218c3e1cf0861c530354e434d
GoGradually <chkun3109@gmail.com>
2025-02-17 00:28:27 +0900

    [FD-306] fix: 피드백 사용 가능 횟수 초기화 이벤트 핸들러 비동기로 변경

--- back-end/src/main/java/com/feedhanjum/back_end/feedback/event/handler/FeedbackSentHandler.java
+++ back-end/src/main/java/com/feedhanjum/back_end/feedback/event/handler/FeedbackSentHandler.java
@@ -4,6 +4,7 @@ import com.feedhanjum.back_end.feedback.event.FeedbackSentEvent;
 import com.feedhanjum.back_end.feedback.service.FeedbackRefineService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 
@@ -14,6 +15,7 @@ public class FeedbackSentHandler {
     private final FeedbackRefineService feedbackRefineService;
 
     @EventListener
+    @Async
     public void on(FeedbackSentEvent event) {
         feedbackRefineService.resetRefineCount(event.senderId());
     }
