cdcf2cf58de82822e608c6a4f6a696dd0ec777be
GoGradually <chkun3109@gmail.com>
2025-02-17 00:11:56 +0900

    [FD-306] fix: ControllerAdvice Order 설정

--- back-end/src/main/java/com/feedhanjum/back_end/feedback/exception/ExternalApiControllerAdvice.java
+++ back-end/src/main/java/com/feedhanjum/back_end/feedback/exception/ExternalApiControllerAdvice.java
@@ -3,6 +3,7 @@ package com.feedhanjum.back_end.feedback.exception;
 import com.feedhanjum.back_end.event.EventPublisher;
 import com.feedhanjum.back_end.feedback.event.FeedbackRefineCountCompensationEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +19,7 @@ public class ExternalApiControllerAdvice {
         this.eventPublisher = eventPublisher;
     }
 
+    @Order(10)
     @ExceptionHandler(ApiResponseFailException.class)
     public ResponseEntity<String> handleApiResponseFailException(ApiResponseFailException e) {
         log.warn("API 호출 실패: ", e.getCause());
@@ -25,6 +27,7 @@ public class ExternalApiControllerAdvice {
         return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(e.getMessage());
     }
 
+    @Order(1)
     @ExceptionHandler(AiRefineChanceAlreadyUsedException.class)
     public ResponseEntity<String> handleAiRefineChanceAlreadyUsedException(AiRefineChanceAlreadyUsedException e) {
         return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body(e.getMessage());
