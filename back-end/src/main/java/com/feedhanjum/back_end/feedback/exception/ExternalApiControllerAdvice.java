799bbbd8770358cec4d99575ffa89352e9efc764
GoGradually <chkun3109@gmail.com>
2025-02-16 22:02:47 +0900

    [FD-306] feat: 보상 로직 이벤트를 발행하는 Controller Advice 생성

---
+++ back-end/src/main/java/com/feedhanjum/back_end/feedback/exception/ExternalApiControllerAdvice.java
@@ -0,0 +1,27 @@
+package com.feedhanjum.back_end.feedback.exception;
+
+import com.feedhanjum.back_end.event.EventPublisher;
+import com.feedhanjum.back_end.feedback.event.FeedbackRefineCountCompensationEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExternalApiControllerAdvice {
+
+    private final EventPublisher eventPublisher;
+
+    public ExternalApiControllerAdvice(EventPublisher eventPublisher) {
+        this.eventPublisher = eventPublisher;
+    }
+
+    @ExceptionHandler(ApiResponseFailException.class)
+    public ResponseEntity<String> handleApiResponseFailException(ApiResponseFailException e) {
+        log.warn("API 호출 실패: ", e.getCause());
+        eventPublisher.publishEvent(new FeedbackRefineCountCompensationEvent(e.getCallerId()));
+        return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(e.getMessage());
+    }
+}
