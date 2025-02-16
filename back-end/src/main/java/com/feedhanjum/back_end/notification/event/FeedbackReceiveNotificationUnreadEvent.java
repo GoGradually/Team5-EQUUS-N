40948602dd74614f4da6f98d86b2eabc6c8f997e
vvsos1 <vvsos10@naver.com>
2025-02-16 23:48:48 +0900

    [FD-363] feat: 미확인 피드백 알림 생성 로직 추가

--- back-end/src/main/java/com/feedhanjum/back_end/notification/event/FeedbackReceiveNotificationUnreadEvent.java
+++
@@ -1,4 +0,0 @@
-package com.feedhanjum.back_end.notification.event;
-
-public record FeedbackReceiveNotificationUnreadEvent(Long notificationId) {
-}
--- back-end/src/main/java/com/feedhanjum/back_end/notification/repository/InAppNotificationQueryRepository.java
+++ back-end/src/main/java/com/feedhanjum/back_end/notification/repository/InAppNotificationQueryRepository.java
@@ -1,11 +1,14 @@
 package com.feedhanjum.back_end.notification.repository;
 
+import com.feedhanjum.back_end.notification.domain.FeedbackReceiveNotification;
 import com.feedhanjum.back_end.notification.domain.InAppNotification;
+import com.feedhanjum.back_end.notification.domain.QFeedbackReceiveNotification;
 import com.feedhanjum.back_end.notification.domain.QInAppNotification;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -20,6 +23,15 @@ public class InAppNotificationQueryRepository {
                 .from(notification)
                 .where(notification.receiverId.eq(receiverId))
                 .orderBy(notification.id.desc())
+                .fetch();
+    }
+
+    public List<FeedbackReceiveNotification> getUnreadFeedbackReceiveNotifications(LocalDateTime from, LocalDateTime to) {
+        QFeedbackReceiveNotification feedbackNotification = QFeedbackReceiveNotification.feedbackReceiveNotification;
+        return queryFactory.select(feedbackNotification)
+                .from(feedbackNotification)
+                .where(feedbackNotification.isRead.isFalse())
+                .where(feedbackNotification.createdAt.between(from, to))
                 .fetch();
     }
 }
