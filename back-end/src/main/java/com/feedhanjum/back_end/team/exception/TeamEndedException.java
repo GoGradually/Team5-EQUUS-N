4355cd143c9c4425a7eac2035bc718869f7e398e
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-02-17 16:51:35 +0900

    [FD-373] feat: 팀이 이미 종료됐을 때 발생하는 예외 정의

---
+++ back-end/src/main/java/com/feedhanjum/back_end/team/exception/TeamEndedException.java
@@ -0,0 +1,7 @@
+package com.feedhanjum.back_end.team.exception;
+
+public class TeamEndedException extends RuntimeException {
+    public TeamEndedException(String message) {
+        super(message);
+    }
+}
