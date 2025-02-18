515f648b957b3c5c47285379cee74b424f140ea7
hammsik <asbbrp@gmail.com>
2025-02-18 11:26:37 +0900

    [FD-375] style: 피드백 리포트 레이아웃 개선

--- front-end/src/pages/mypage/components/ReportTopKeywords.jsx
+++ front-end/src/pages/mypage/components/ReportTopKeywords.jsx
@@ -14,7 +14,7 @@ export default function ReportTopKeywords({ topKeywords }) {
   return (
     <div className='flex flex-col gap-3'>
       <h2 className='header-4 ml-1 text-gray-100'>가장 많이 받은 키워드</h2>
-      <div className='flex gap-3'>
+      <div className='flex w-full gap-3'>
         {topKeywords.map((keyword, i) => (
           <Card key={i} keyword={keyword} />
         ))}
@@ -30,11 +30,11 @@ export default function ReportTopKeywords({ topKeywords }) {
 const Card = ({ keyword }) => {
   console.log(keyword);
   return (
-    <div className='rounded-400 flex flex-1 flex-col items-center justify-between gap-6 bg-gray-800 p-5'>
+    <div className='rounded-400 flex w-full flex-col items-center justify-between gap-6 bg-gray-800 p-5'>
       <div className='text-3xl'>
         {keyword.feeling === '아쉬워요' ? '🤔' : '😊'}
       </div>
-      <p className='subtitle-2 text-center break-keep whitespace-pre-line text-gray-100'>{`“\n${keyword.keyword}\n”`}</p>
+      <p className='subtitle-2 text-center break-keep whitespace-pre-line text-gray-100'>{`❝\n${keyword.keyword}\n❞`}</p>
       <div
         className={`rounded-100 caption-2 size-fit ${keyword.feeling === '아쉬워요' ? 'bg-[#FFA3A3]/20 text-[#FFA3A3]' : 'bg-[#91CBFB]/20 text-[#91CBFB]'} px-2 py-1`}
       >
