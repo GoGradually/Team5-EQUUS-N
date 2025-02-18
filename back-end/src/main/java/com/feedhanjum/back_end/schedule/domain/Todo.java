687f854306781c69899b377ad419eae64bc3595d
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-02-18 09:49:32 +0900

    [FD-30] feat: Todo 글자수 제한 적용 - 50자

--- back-end/src/main/java/com/feedhanjum/back_end/schedule/domain/Todo.java
+++ back-end/src/main/java/com/feedhanjum/back_end/schedule/domain/Todo.java
@@ -4,12 +4,15 @@ import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Size;
+
 @Embeddable
 @NoArgsConstructor
 @Getter
 public class Todo {
     private String content;
 
+    @Size(min = 1, max = 50)
     public Todo(String content) {
         this.content = content;
     }
