832a13cd0e30bf384fb33809c87cb5c6cba79771
hammsik <asbbrp@gmail.com>
2025-02-20 16:53:53 +0900

    [FD-398] feat: 피드백 보내기 step 2 에니메이션 stagger 사용 ver

--- front-end/src/pages/feedback/components/FeedbackSend2.jsx
+++ front-end/src/pages/feedback/components/FeedbackSend2.jsx
@@ -1,10 +1,11 @@
-import { useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useFeedbackObjective } from '../../../api/useFeedback';
 import KeywordButton from '../../../components/buttons/KeywordButton';
 import { showToast } from '../../../utility/handleToast';
 import FooterWrapper from '../../../components/wrappers/FooterWrapper';
 import LargeButton from '../../../components/buttons/LargeButton';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { AnimatePresence, motion } from 'motion/react';
 
 export default function FeedbackSend2() {
   const navigate = useNavigate();
@@ -13,6 +14,19 @@ export default function FeedbackSend2() {
   const { data: objectives } = useFeedbackObjective();
 
   const [selectedObjectives, setSelectedObjectives] = useState([]);
+  const [step, setStep] = useState(2);
+
+  useEffect(() => {
+    setTimeout(() => {
+      selectedObjectives.length > 0 &&
+        navigate('../3', {
+          state: {
+            ...locationState,
+            objectiveFeedbacks: selectedObjectives,
+          },
+        });
+    }, 500);
+  }, [step]);
 
   const onKeywordButtonClick = (keyword) => {
     selectedObjectives.includes(keyword) ?
@@ -24,52 +38,83 @@ export default function FeedbackSend2() {
     : showToast('키워드는 5개까지 선택 가능해요');
   };
 
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: 'spring',
+        bounce: 0.32,
+      },
+    },
+  };
+
   return (
     <div className='flex size-full flex-col gap-8'>
       <h1 className='header-2 text-gray-0 mt-3 whitespace-pre-line'>
         {'보낼 피드백 키워드를\n선택해 주세요'}
       </h1>
-      {objectives && (
-        <ul className='scrollbar-hidden flex w-full gap-8 overflow-x-auto p-1 whitespace-nowrap'>
-          {Object.keys(objectives[locationState.feedbackFeeling]).map(
-            (title, index) => (
-              <ObjectiveColumn
-                key={index}
-                title={title}
-                keywords={objectives[locationState.feedbackFeeling][title]}
-                selectedKeywords={selectedObjectives}
-                onClick={onKeywordButtonClick}
-              />
-            ),
-          )}
-        </ul>
-      )}
+      <AnimatePresence>
+        {step === 2 && objectives && (
+          <motion.ul
+            className='scrollbar-hidden flex w-full gap-8 overflow-x-auto p-1 whitespace-nowrap'
+            variants={containerVariants}
+            initial='hidden'
+            animate='show'
+          >
+            {Object.keys(objectives[locationState.feedbackFeeling]).map(
+              (title, index) => (
+                <ObjectiveColumn
+                  key={index}
+                  title={title}
+                  keywords={objectives[locationState.feedbackFeeling][title]}
+                  selectedKeywords={selectedObjectives}
+                  onClick={onKeywordButtonClick}
+                  itemVariants={itemVariants}
+                />
+              ),
+            )}
+          </motion.ul>
+        )}
+      </AnimatePresence>
       <FooterWrapper>
         <LargeButton
           isOutlined={false}
           text='다음'
           disabled={selectedObjectives.length === 0}
-          onClick={() =>
-            selectedObjectives.length > 0 &&
-            navigate('../3', {
-              state: {
-                ...locationState,
-                objectiveFeedbacks: selectedObjectives,
-              },
-            })
-          }
+          onClick={() => setStep(3)}
         />
       </FooterWrapper>
     </div>
   );
 }
 
-const ObjectiveColumn = ({ title, keywords, selectedKeywords, onClick }) => {
-  console.log(keywords);
+const ObjectiveColumn = ({
+  title,
+  keywords,
+  selectedKeywords,
+  onClick,
+  itemVariants,
+}) => {
   return (
-    <li className='inline-block w-80'>
+    <motion.li
+      className='inline-block w-80'
+      variants={itemVariants}
+      exit={{ opacity: 0, x: -30 }}
+    >
       <h2 className='subtitle-1 text-gray-0 mb-3'>{title}</h2>
-      <div className='flex flex-col gap-2'>
+      <ul className='flex flex-col gap-2'>
         {keywords.map((keyword, index) => (
           <KeywordButton
             key={index}
@@ -79,7 +124,7 @@ const ObjectiveColumn = ({ title, keywords, selectedKeywords, onClick }) => {
             {keyword}
           </KeywordButton>
         ))}
-      </div>
-    </li>
+      </ul>
+    </motion.li>
   );
 };
