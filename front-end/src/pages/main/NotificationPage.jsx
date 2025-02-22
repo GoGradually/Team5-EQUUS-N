2f370a37a8992d8976d0efb579bfe8fc36598ca9
Se Jun Lim <lsj1137@naver.com>
2025-02-22 11:41:38 +0900

    [FD-399] feat: 알림 모두 읽음 처리

--- front-end/src/pages/main/NotificationPage.jsx
+++ front-end/src/pages/main/NotificationPage.jsx
@@ -4,6 +4,7 @@ import NavBar2 from '../../components/NavBar2';
 import StickyWrapper from '../../components/wrappers/StickyWrapper';
 import Alarm from './components/Alarm';
 import { useNavigate } from 'react-router-dom';
+import Tag from '../../components/Tag';
 export default function NotificationPage() {
   const [selectedTeamId, setSelectedTeamId] = useState(1);
   const { data: notificationsData, markAsRead } =
@@ -13,7 +14,6 @@ export default function NotificationPage() {
 
   useEffect(() => {
     if (notificationsData) {
-      console.log(notificationsData);
       setNotifications(notificationsData);
     }
   }, [notificationsData]);
@@ -31,17 +31,30 @@ export default function NotificationPage() {
         />
         <div className='border-b border-gray-700'></div>
       </StickyWrapper>
-      <h2 className='subtitle-1 text-gray-0 mt-6 mb-2'>새로운 알림</h2>
+      <div className='mt-6 mb-2 flex justify-between'>
+        <h2 className='subtitle-1 text-gray-0'>새로운 알림</h2>
+        <button
+          onClick={() =>
+            markAsRead({
+              notificationIds: [
+                ...notifications.map((noti) => noti.notificationId),
+              ],
+            })
+          }
+        >
+          <Tag type='TEAM_NAME'>모두 읽음</Tag>
+        </button>
+      </div>
       <ul className='h-full'>
         {notifications.length > 0 ?
-          notifications.map((notification) => (
+          notifications.map((noti) => (
             <li
-              key={notification.notificationId}
+              key={noti.notificationId}
               onClick={() =>
-                markAsRead({ notificationIds: [notification.notificationId] })
+                markAsRead({ notificationIds: [noti.notificationId] })
               }
             >
-              <Alarm type={notification.type} data={notification} />
+              <Alarm type={noti.type} data={noti} />
             </li>
           ))
         : <div className='flex h-full flex-col items-center justify-center gap-4 text-gray-300'>
