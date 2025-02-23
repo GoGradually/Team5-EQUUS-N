9d23d77115d022b779c1802c4cdc8329824c07f6
hammsik <asbbrp@gmail.com>
2025-02-23 11:41:26 +0900

    [FD-410] feat: motion 활용한 라우팅 애니메이션 (exit 안됨)

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
 import 'react-datepicker/dist/react-datepicker.css';
 import './index.css';
 import './customDatePicker.css';
@@ -32,6 +32,7 @@ import FeedbackSendFreq from './pages/feedback/FeedbackSendFreq';
 import ProfileEdit from './pages/mypage/ProfileEdit';
 import Report from './pages/mypage/Report';
 import PasswordReset from './pages/auth/PasswordReset';
+import { motion, AnimatePresence } from 'motion/react';
 
 const queryClient = new QueryClient();
 
@@ -40,59 +41,89 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <CombinedProvider>
-          <Routes>
-            <Route element={<Layout />}>
-              <Route path='/:teamCode?' element={<Splash />} />
-              <Route path='/login/google' element={<SplashForOAuth />} />
-              <Route path='signin' element={<SignIn />} />
-              <Route path='signup' element={<SignUp />} />
-              <Route path='password/reset' element={<PasswordReset />} />
-              {/* 이 아래는 로그인 해야 이용 가능 */}
-              <Route element={<ProtectedRoute />}>
-                <Route path='feedback'>
-                  <Route path='request' element={<FeedbackRequest />} />
-                  <Route path='send' element={<FeedbackSendLayout />}>
-                    <Route index element={<FeedbackSend />} />
-                    <Route path='frequent' element={<FeedbackSendFreq />} />
-                    <Route path=':step' element={<FeedbackSendStep />} />
-                  </Route>
-                  <Route path='self' element={<FeedbackSelf />} />
-                  <Route path='complete' element={<FeedbackComplete />} />
-                  <Route path='favorite' element={<FeedbackFavorite />} />
-                  <Route path='received' element={<FeedbackHistory />} />
-                  <Route path='sent' element={<FeedbackHistory />} />
-                </Route>
-                <Route path='teamspace'>
-                  <Route path='make'>
-                    <Route index element={<TeamSpaceMake />} />
-                    <Route
-                      path='first'
-                      element={<TeamSpaceMake isFirst={true} />}
-                    />
-                    <Route path='success' element={<TeamSpaceMakeSuccess />} />
-                  </Route>
-                  <Route path='list' element={<TeamSpaceList />} />
-                  <Route path='manage/:teamId'>
-                    <Route index element={<TeamSpaceManage />} />
-                    <Route path='edit' element={<TeamSpaceEdit />} />
-                  </Route>
-                </Route>
-                <Route path='calendar' element={<Calendar />} />
-                <Route path='main'>
-                  <Route index element={<MainPage />} />
-                  <Route path='notification' element={<NotificationPage />} />
-                </Route>
-                <Route path='mypage'>
-                  <Route index element={<MyPageHome />} />
-                  <Route path='self' element={<FeedbackHistory />} />
-                  <Route path='report' element={<Report />} />
-                  <Route path='edit' element={<ProfileEdit />} />
-                </Route>
-              </Route>
-            </Route>
-          </Routes>
+          <AnimatedRoutes />
         </CombinedProvider>
       </BrowserRouter>
     </QueryClientProvider>
+  );
+}
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode='wait'>
+      <Routes key={location.pathname}>
+        <Route element={<Layout />}>
+          <Route path='/:teamCode?' element={<Splash />} />
+          <Route path='/login/google' element={<SplashForOAuth />} />
+          <Route path='signin' element={<SignIn />} />
+          <Route path='signup' element={<SignUp />} />
+          <Route path='password/reset' element={<PasswordReset />} />
+          {/* 이 아래는 로그인 해야 이용 가능 */}
+          <Route element={<ProtectedRoute />}>
+            <Route path='feedback'>
+              <Route path='request' element={<FeedbackRequest />} />
+              <Route path='send' element={<FeedbackSendLayout />}>
+                <Route index element={<FeedbackSend />} />
+                <Route path='frequent' element={<FeedbackSendFreq />} />
+                <Route path=':step' element={<FeedbackSendStep />} />
+              </Route>
+              <Route path='self' element={<FeedbackSelf />} />
+              <Route path='complete' element={<FeedbackComplete />} />
+              <Route path='favorite' element={<FeedbackFavorite />} />
+              <Route path='received' element={<FeedbackHistory />} />
+              <Route path='sent' element={<FeedbackHistory />} />
+            </Route>
+            <Route path='teamspace'>
+              <Route path='make'>
+                <Route index element={<TeamSpaceMake />} />
+                <Route
+                  path='first'
+                  element={<TeamSpaceMake isFirst={true} />}
+                />
+                <Route path='success' element={<TeamSpaceMakeSuccess />} />
+              </Route>
+              <Route path='list' element={<TeamSpaceList />} />
+              <Route path='manage/:teamId'>
+                <Route index element={<TeamSpaceManage />} />
+                <Route path='edit' element={<TeamSpaceEdit />} />
+              </Route>
+            </Route>
+            <Route path='calendar' element={<Calendar />} />
+            <Route path='main'>
+              <Route index element={<MainPage />} />
+              <Route
+                path='notification'
+                element={
+                  <PageWrapper>
+                    <NotificationPage />
+                  </PageWrapper>
+                }
+              />
+            </Route>
+            <Route path='mypage'>
+              <Route index element={<MyPageHome />} />
+              <Route path='self' element={<FeedbackHistory />} />
+              <Route path='report' element={<Report />} />
+              <Route path='edit' element={<ProfileEdit />} />
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
+function PageWrapper({ children }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: 50 }} // 처음 로드될 때의 상태
+      animate={{ opacity: 1, x: 0 }} // 활성화될 때
+      exit={{ opacity: 0, x: -50 }} // 제거될 때
+      transition={{ duration: 0.3 }}
+    >
+      {children}
+    </motion.div>
   );
 }
